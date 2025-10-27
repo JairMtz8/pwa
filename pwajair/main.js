d26e58928278cf31698884e7d9993e79d4f83873
Jair Antonio Martínez Valladares <20223tn068@utez.edu.mx>
2025-10-27 12:29:42 -0600

    pwa

--- pwajair/main.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
-        .then(reg => {
-            console.log("Service Worker registrado correctamente ", reg);
-        })
-        .catch(err => console.error('Service Worker registration failed', err))
-    });
-}
